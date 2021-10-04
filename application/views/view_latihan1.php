62f11b7d559ee95a07bfe8a4cc1c0ecd574837d0
HasnaAulia16 <hasnaaulia1610@gmail.com>
2021-10-04 11:07:27 +0700

    Upload Tugas Pertemuan 2

---
+++ application/views/view_latihan1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Latihan1</title>
+</head>
+<body>
+	Halo Sahabat Joy...yuk belajar web programming! <br>
+	Nilai 1 = <?= $nilai1;?>
+	Nilai 2 = <?= $nilai2;?>
+	ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+     $nilai1 . " + ". $nilai2. " = ".$hasil; ?>
+</body>
+</html>
