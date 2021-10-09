e4e875ec9a9bc38d351d0f18848458022c068c08
HasnaAulia16 <hasnaaulia1610@gmail.com>
2021-10-09 21:30:19 +0700

    Tugas Pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Hasna</li>
+ <li>Nama Belakang : Aulia</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Rusdarmoyo Komplek ATS Blok C1 No.5 Kec.Kemang Kab.Bogor </li>
+ </ul>
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Pati</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Bulutangkis</li>
+ <li>Lari</li>
+ </ul>
+</section>
